73744c633cd08a7c487d83fb8fb8165f494899cf
Kevin Nobles <feuvoreve-nightmare@hotmail.com>
2017-12-16 21:48:54 +0100

    Calendar.c c'est pour le calendrier et dans med.c il renvoie ceux qui sont actifs maintenant

--- CProject/med.c
+++ CProject/med.c
@@ -10,15 +10,27 @@ struct meds
 	char inami[15],spec[21],prenom[31],nom[31],start[9],end[9];
 };
 
+typedef strict pats patds;
+struct pats
+{
+	struct pats *suivant;
+	char nom[31], prenom[31], niss[14], dateNaissance[11];
+	int age;
+}
+
 int main()
 {
-	FILE*fdat;
+	FILE*fdat,*fpat;
+	fpat = fopen("patients.dat","r");
 	fdat = fopen("medecin.dat","r");
-
-	meds *tete, *courant, *medsuivant;
+	pats *ptete,*pcourannt,*psuivant,*pfirst;
+	meds *tete, *courant, *medsuivant,*medfirst;
+	pcourant = malloc(sizeof(pat));
 	courant = malloc(sizeof(meds));
 	int i,j;
 	tete = courant;	
+	medfirst = courant;
+	
 	fscanf(fdat,"%14s",courant->inami);
 	while(!feof(fdat))
 	{
@@ -36,5 +48,32 @@ int main()
 		printf("%-14s %-20s %-30s %-30s %-8s %-8s\n",courant->inami,courant->spec,courant->prenom,courant->nom,courant->start,courant->end);
 		courant = courant->suivant;
 	}
+	int p;
+	
+	
+	int ret1;
+	int ret2;
 
+	printf("Medecins disponibles maintenant\n");
+	printf("-------------------------------\n\n");
+	
+	time_t rawtime;
+	struct tm *tminfo;
+	time(&rawtime);
+	tminfo = localtime(&rawtime);
+	char timeString[9];
+	strftime(timeString,sizeof(timeString),"%H:%M:%S", tminfo);
+	printf("%s\n",timeString);
+	
+	courant = medfirst;
+	for(i = 0; i<=j;i++)
+	{
+		ret1 = strcmp(timeString,courant->start);
+		ret2 = strcmp(timeString,courant->end);
+		if(ret1 > 0 && ret2 < 0)
+		{
+			printf("%-14s %-20s %-30s %-30s %-8s %-8s\n",courant->inami,courant->spec,courant->prenom,courant->nom,courant->start,courant->end);
+		}
+		courant = courant->suivant;
+	}	
 }
